4ee51d635b97698572b84ce999d982f679ea16c2
Jack Hamel <jackhaml16@gmail.com>
2018-01-27 20:57:24 -0500

    WIP

--- homework/hw1.py
+++ homework/hw1.py
@@ -1,22 +1,30 @@
 import numpy as np
 
-N = 1000
+N = 10
 n = np.linspace(0, 2*np.pi-2*np.pi/N, N)
-x,y = np.array(np.cos(n)), np.array(np.sin(n))
+x,y = np.array(cos(n)), np.array(sin(n))
 
-A = np.matrix([np.zeros(N) for i in range(N)])
+A = np.array([zeros(N) for i in range(N)])
 for i in range(N):
     for j in range(N):
-        x_dist, y_dist = abs(x[i]-x[j]), abs(y[i]-y[j])
         if (i==j):
             A[i,j] = 1
             continue
+        x_dist, y_dist = abs(x[i]-x[j]), abs(y[i]-y[j])
         A[i,j] = 1 / np.sqrt(x_dist**2+y_dist**2)
         
+#def test1(A,x,N):
+    
 
-x = np.ones(N)
-xft = np.fft.fft(x)
-Aft = np.fft.fft2(A)
-b = np.fft.ifft2(np.inner(diag(xft),Aft))
+
+
+x = np.random.rand(N)
+a = np.array(A[:,0])
+aft, xft = np.fft.fft(a), np.fft.fft(x)
+b = fft.ifft(np.inner(np.diag(aft),xft))
+
+
+#Aft, xft = np.fft.fft2(A), np.fft.fft(x)
+#b = np.fft.ifft2(np.inner(np.diag(xft),Aft))
 
 b2 = np.inner(A,x)
