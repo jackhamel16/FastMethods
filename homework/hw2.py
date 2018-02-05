478e3c57831f0951dfa97030143072d20cd29e13
Jack Hamel <jackhaml16@gmail.com>
2018-02-05 18:51:50 -0500

    stuff

--- homework/hw2.py
+++ homework/hw2.py
@@ -1,45 +1,56 @@
 import time
 import numpy as np
+import scipy.linalg as lg
 import matplotlib.pyplot as plt
 import copy
 
-size = 3
-N = size**2
+row,col = 3,20
+N = row*col
 
-def idx2coord(i,N):
-    row = i%np.sqrt(N)
-    col = np.floor(i/np.sqrt(N))
-    return(int(col), int(row))
+#def idx2coord(i,N):
+#    row = i%np.sqrt(N)
+#    col = np.floor(i/np.sqrt(N))
+#    return(int(col), int(row))
+
+def idx2coord(i,c):
+    row = np.floor(i/c)
+    col = i%c
+    return(int(row), int(col))
     
-g_mat = np.array([np.zeros(size**2) for i in range(size**2)])
-x_mat = np.array([np.zeros(size) for i in range(size)])
-y_mat = copy.copy(x_mat)
+g_mat = np.array([np.zeros(N) for i in range(N)])
 
 N_vec = np.array([i for i in range(N)])
 for i in range(N):
-    x_src, y_src = idx2coord(i, N)
-#    print(x_src,y_src)
-    for j in N_vec:
-#        if(i==2):
-#            print(x_obs,y_obs)
-        x_obs, y_obs = idx2coord(j, N)
-#        if(i==j):
-#            g_mat[i,j] = 1
-#            continue
+    x_src, y_src = idx2coord(i, col)
+    for j in range(N):
+        x_obs, y_obs = idx2coord(j, col)
         g_mat[i,j] = (np.sqrt((x_obs-x_src)**2+(y_obs-y_src)**2))
-#    N_vec = np.roll(N_vec,-1)
-#    print(N_vec)
+        
+
+g_mat2 = np.array([np.zeros(col**2) for i in range(row**2)])
+
+blocks = []
+for i in range(row):
+    dist = np.zeros(col)
+    for x in range(col):
+        dist[x] = np.sqrt((x - 0)**2 + (0 - i)**2)
+    tpz = lg.toeplitz(dist, dist)
+    blocks.append(tpz)
+    
+#    for y in range(row):
+#        dist[]
+
 
 # FIGURE OUT HOW TO GO FROM TOEPLITZ -> CIRCULENT
 
-g_vec = np.zeros(2*N)
-g_vec[0:N] = g_mat[0,:]
-g_vec[N:2*N] = g_mat[1,:]       
-
-circ_mat = np.array([np.zeros(2*size**2) for i in range(2*size**2)])
-for i in range(2*N):
-    circ_mat[i,:] = g_vec
-    g_vec = np.roll(g_vec,1)
+#g_vec = np.zeros(2*N)
+#g_vec[0:N] = g_mat[0,:]
+#g_vec[N:2*N] = g_mat[1,:]       
+#
+#circ_mat = np.array([np.zeros(2*size**2) for i in range(2*size**2)])
+#for i in range(2*N):
+#    circ_mat[i,:] = g_vec
+#    g_vec = np.roll(g_vec,1)
 
 #circ_mat[0:N,0:N] = copy.copy(g_mat)
 #circ_mat[0:N,N:2*N] = copy.copy(g_mat)
