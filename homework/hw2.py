be4dbd9741a962f910f0ce42f50c34a00cff3404
Jack Hamel <jackhaml16@gmail.com>
2018-02-05 19:41:03 -0500

    stuff

--- homework/hw2.py
+++ homework/hw2.py
@@ -4,7 +4,7 @@ import scipy.linalg as lg
 import matplotlib.pyplot as plt
 import copy
 
-row,col = 3,20
+row,col = 2,3 # row x row blocks of col x col toeplitz matrices
 N = row*col
 
 #def idx2coord(i,N):
@@ -12,57 +12,40 @@ N = row*col
 #    col = np.floor(i/np.sqrt(N))
 #    return(int(col), int(row))
 
-def idx2coord(i,c):
+def idx2coord(i, c):
     row = np.floor(i/c)
     col = i%c
-    return(int(row), int(col))
+    return(row, col)
+    
+def fft_solve(a, x):
+    aft, xft = np.fft.fft(a), np.fft.fft(x)
+    return(np.fft.ifft(aft*xft))
     
 g_mat = np.array([np.zeros(N) for i in range(N)])
 
-N_vec = np.array([i for i in range(N)])
 for i in range(N):
     x_src, y_src = idx2coord(i, col)
     for j in range(N):
+        if(i==j):
+            g_mat[i,j] = 1
+            continue
         x_obs, y_obs = idx2coord(j, col)
-        g_mat[i,j] = (np.sqrt((x_obs-x_src)**2+(y_obs-y_src)**2))
-        
+        g_mat[i,j] = 1/(np.sqrt((x_obs-x_src)**2+(y_obs-y_src)**2))
 
-g_mat2 = np.array([np.zeros(col**2) for i in range(row**2)])
+blk_dim, tpz_dim = row, col
+x = np.ones(N)
 
-blocks = []
-for i in range(row):
-    dist = np.zeros(col)
-    for x in range(col):
-        dist[x] = np.sqrt((x - 0)**2 + (0 - i)**2)
-    tpz = lg.toeplitz(dist, dist)
-    blocks.append(tpz)
-    
-#    for y in range(row):
-#        dist[]
+t0 = g_mat[0:col,0:col]
+t0_vec = np.hstack((t0[0,0:col], t0[-1,0:col]))
+t0_circ = lg.circulant (t0_vec)
+t0_0 = fft_solve(t0_vec, x[0:2*col])[0:col]
 
+t1 = g_mat[col:2*col]
+t1_vec = np.hstack((t1[0,0:col], t1[-1,0:col]))
+t1_0 = fft_solve(t1_vec, x[0:2*col])[col:2*col]
 
-# FIGURE OUT HOW TO GO FROM TOEPLITZ -> CIRCULENT
-
-#g_vec = np.zeros(2*N)
-#g_vec[0:N] = g_mat[0,:]
-#g_vec[N:2*N] = g_mat[1,:]       
-#
-#circ_mat = np.array([np.zeros(2*size**2) for i in range(2*size**2)])
-#for i in range(2*N):
-#    circ_mat[i,:] = g_vec
-#    g_vec = np.roll(g_vec,1)
-
-#circ_mat[0:N,0:N] = copy.copy(g_mat)
-#circ_mat[0:N,N:2*N] = copy.copy(g_mat)
-#circ_mat[N:2*N,0:N] = copy.copy(g_mat)
-#circ_mat[N:2*N,N:2*N] = copy.copy(g_mat)
-
+x = np.ones(N)
+b = np.inner(g_mat,x)
 
 
 plt.imshow(g_mat, cmap='hot', interpolation='nearest')
-
-#plt.imshow(circ_mat, cmap='hot', interpolation='nearest')
-#for i in range(2*N):
-#    for j in range(2*N):
-#        print("{a:2.2f}".format(a=circ_mat[i,j]), end= "  ")
-#    print("\n")
