deb9c5da2da85d11cc95a80d687aa4b65f8f9f33
Jack Hamel <jackhaml16@gmail.com>
2018-02-23 00:02:39 -0500

    added featuers. still doesnt work right tho :(

--- homework/hw2_attempt2.py
+++ homework/hw2_attempt2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import copy
 import random
 
-row,col = 4,4 # row x row blocks of col x col toeplitz matrices
+row,col = 3,2 # row x row blocks of col x col toeplitz matrices
 N = row*col
 
 def idx2coord(i, c):
@@ -46,6 +46,23 @@ def compute_blocks(row, col):
         blocks.append(block)
     return(blocks)
 
+def compute_rowcol(row, col):
+    rows = []
+    cols = []
+    for y2 in range(row):
+        b_row = np.zeros(col)
+        b_col = np.zeros(col)
+        for x2 in range(col):
+            if((x2 == 0) & (y2==0)):
+                b_row[x2] = 1
+                b_col[x2] = 0
+                continue;
+            b_row[x2] = 1 / np.sqrt((x2)**2 + (y2)**2)
+            b_col[x2] = 1 / np.sqrt((-x2)**2 + (-y2)**2)
+        rows.append(b_row)
+        cols.append(b_col)
+    return(rows, cols)
+
 def create_circ_list(blocks):
     add_indices = np.roll(np.flip(np.array(range(len(blocks))), 0), 1)
     blocks_circ = copy.copy(blocks)
@@ -53,7 +70,7 @@ def create_circ_list(blocks):
         blocks_circ.append(blocks[i])
     return(blocks_circ)
 
-def toeplitz2circulant(tp_block):
+def toeplitz2circulant_b(tp_block):
     tp2_row = np.hstack((0, np.flip(tp_block[1:tp_block.shape[1],0], 0)))
     tp2_col = np.hstack((0, np.flip(tp_block[0,1:tp_block.shape[1]], 0)))
     tp2_block = lg.toeplitz(tp2_row, tp2_col)
@@ -61,11 +78,43 @@ def toeplitz2circulant(tp_block):
     h1 = np.hstack((tp2_block, tp_block))
     return(np.vstack((h0,h1)))
 
+def toeplitz2circulant_r(row, col):
+#    row is row of toeplitz mat
+#    col is col of toeplitz mat
+    work_col = np.roll(np.flip(col,0),1)
+    work_col[0] = 0
+    return(np.hstack((row,work_col)))
+
+#note: code works for isotropic Green's kernels ONLY
+rows, cols = compute_rowcol(row, col)
 blocks = compute_blocks(row, col)       
 g_mat = compute_g1(row, col)
+
 x = np.random.rand(N)
+b = np.inner(g_mat,x)
+
 # compute circulant matrices of tpz blocks
-circ_blocks = [toeplitz2circulant(block) for block in blocks]
+circ_blocks = [toeplitz2circulant_b(block) for block in blocks]
+circ_rows = [toeplitz2circulant_r(rows[i], cols[i]) for i in range(row)]
+circ = np.hstack(tuple(circ_rows))
+# Create top row of circulant block circulant matrix
+pad_array = np.zeros(2*col)
+work_rows = np.roll(np.flip(np.hstack(tuple(circ_rows)),0),2*col)
+work_rows[0:2*col] = np.zeros(2*col)
+block_circ_row = np.hstack((np.hstack(tuple(circ_rows))\
+                             ,work_rows))
+
+# Diagnolize block_circ_row
+diag = np.hstack(tuple((np.fft.fft2(np.reshape(block_circ_row,(2*col,2*row))))))
+x_pad = np.zeros(3*N)
+x_padded = np.hstack((x,x_pad))
+b_fast = np.fft.ifft(diag*np.fft.fft(x_padded))
+
+
+circ_row = np.array([])
+for block in circ_blocks:
+    circ_row = np.hstack((circ_row, block[0,:]))
+    
 
 
 
