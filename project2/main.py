be4fec60fe7f3e6c33a7eca8cf84e5223c36ab1a
Jack Hamel <jackhaml16@gmail.com>
2018-03-25 16:43:01 -0400

    Added computation of potentials using tree node interactions

--- project2/main.py
+++ project2/main.py
@@ -1,34 +1,82 @@
 import numpy as np
+import numpy.linalg as lg
+import matplotlib.pyplot as plt
 import source
 import tree
 
-level_cnt = 4 # Count levels starting from root = 0 
+level_cnt = 3 # Count levels starting from root = 0 
 grid_dim = 2**(level_cnt-1) # Should remain power of two for easy life
 grid_step = 1
 N = 5
 
 src_list = []
-#src_list = [source.source(4.5,1.5,1), source.source(1.2,2.3,1), \
-#            source.source(7,7.1,1)]
+src_list = [source.source(0.1,0.1,1), source.source(0.4,0.4,1), \
+            source.source(3.5,3.5,1), source.source(2.1,2.1,1), \
+            source.source(3.2,3.2,1)]
 for i in range(N):
-    src_list.append(source.source(grid_dim * np.random.random(),grid_dim * \
-                              np.random.random(), np.random.random()))
+#    src_list.append(source.source(grid_dim * np.random.random(),grid_dim * \
+#                              np.random.random(), np.random.random()))
     # Map src to nearest lower left grid pnt
     src_list[i].grid = (int(np.floor(src_list[i].x/grid_step)), \
                         int(np.floor(src_list[i].y/grid_step)))
     
-linear_tree = tree.tree(src_list, level_cnt)
-linear_tree = linear_tree.build()
+my_tree = tree.tree(src_list, level_cnt)
+my_tree.build()
 
-interactions = tree.interaction(level_cnt)
-interactions.fill_child(2,1,level_cnt-2)
+
+interactions = tree.interaction(level_cnt, my_tree)
 interactions.fill_list()
+interactions.compute_potentials()
+
+#Direct Computation
+idxs = [i for i in range(N)]
+G = interactions.build_G(idxs, idxs)
+src_vec = np.array([src.weight for src in src_list])
+direct_potentials = np.dot(G, src_vec)
 
 
 
+error = (lg.norm(interactions.potentials) - lg.norm(direct_potentials))\
+        / lg.norm(direct_potentials)
+        
+print('Error: ', error)
 
+error_vec = (interactions.potentials - direct_potentials) / direct_potentials
 
+### OLD
+#for obs_box_idx in range(len(interactions.list)):
+#    obs_srcs = my_tree.tree[obs_box_idx]
+#    obs_pot = np.zeros(len(obs_srcs))
+#    #far field interactions
+#    obs_pot = interactions.compute_box_pot_slow(obs_box_idx)
+#    #near field interacitons
+#    obs_pot += interactions.compute_box_pot_slow(obs_box_idx, 1)
+#    for i, obs in enumerate(obs_srcs):
+#        interactions.potentials[obs] += obs_pot[i]
 
+### OLD
+#for obs_box_idx in range(len(interactions.list)):
+#    obs_srcs = my_tree.tree[obs_box_idx]
+#    obs_pot = np.zeros(len(obs_srcs))
+#    
+#    #far field interactions
+#    for src_box_idx in interactions.list[obs_box_idx]:
+#        src_srcs = my_tree.tree[src_box_idx]
+#        src_vec = np.array([src_list[idx].weight for idx in src_srcs])
+#        G = interactions.build_G(obs_srcs, src_srcs)
+#        if np.size(G) != 0:
+#            obs_pot += np.dot(G, src_vec)
+#            pot = np.dot(G, src_vec)
+#    
+#    #near field interacitons
+#    for src_box_idx in interactions.near_list[obs_box_idx]:
+#        src_srcs = my_tree.tree[src_box_idx]
+#        src_vec = np.array([src_list[idx].weight for idx in src_srcs])
+#        G = interactions.build_G(obs_srcs, src_srcs)
+#        if np.size(G) != 0:
+#            obs_pot += np.dot(G, src_vec)
+#    for i, obs in enumerate(obs_srcs):
+#        interactions.potentials[obs] += obs_pot[i]
 
 
 
